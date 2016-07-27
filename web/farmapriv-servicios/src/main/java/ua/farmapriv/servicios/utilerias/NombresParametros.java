fc73003b80d49be2666aae3aa0490f20a6e12996
Adrian Barzola Jupiter <ambj77@gmail.com>
2016-07-26 23:23:59 -0500

    more configurations
    configurations

--- web/farmapriv-servicios/src/main/java/ua/farmapriv/servicios/utilerias/NombresParametros.java
+++ web/farmapriv-servicios/src/main/java/ua/farmapriv/servicios/utilerias/NombresParametros.java
@@ -2,12 +2,11 @@ package ua.farmapriv.servicios.utilerias;
 
 public final class NombresParametros {
 	
-	public static final String NOMBRE_UNIDAD_PERSISTENCIA = "pu_screw";
+	public static final String NOMBRE_UNIDAD_PERSISTENCIA = "pu_farmapriv";
 	public static final String ESTADO_ACTIVO = "A";
 	public static final String ESTADO_INACTIVO = "I";
-	public static final String NOMBRE_DATASOURCE = "java:/screwPoolDatos";
-	public static final String JNDI_SERVICIO_SEGURIDAD = "java:global/screw-servicios/ServicioSeguridad!ug.screw.servicios.servicios.seguridad.IServicioSeguridad";
-	public static final String JNDI_SERVICIO_PARAMETRO = "java:global/screw-servicios/ServicioParametro!ug.screw.servicios.servicios.parametros.IServicioParametro";
-	public static final String JNDI_SERVICIO_COLABORACION = "java:global/screw-servicios/ServicioColaboracion!ug.screw.servicios.servicios.colaboracion.IServicioColaboracion";
+	public static final String NOMBRE_DATASOURCE = "java:/farmaprivPoolDatos";
+	public static final String JNDI_SERVICIO_SEGURIDAD = "java:global/farmapriv-servicios/ServicioSeguridad!ua.farmapriv.servicios.servicios.seguridad.IServicioSeguridad";
+	public static final String JNDI_SERVICIO_PARAMETRO = "java:global/farmapriv-servicios/ServicioParametro!ua.farmapriv.servicios.servicios.parametros.IServicioParametro";
 
 }
